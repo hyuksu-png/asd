c80205d3469087a06091991f6ed2b9025df5862f
dnsgk4983 <dnsgk4983@gmail.com>
2021-02-24 18:13:38 +0900

    v1.0.2

---
+++ js/section02.js
@@ -0,0 +1,337 @@
+
+    $(function(){
+        // 코스 버튼1
+        $(".guidemap-btn01").mouseover(function(){
+            $(".guide-menu01").css("color","#e2de66");
+            $("#guidmap-img01").attr("src","images/mapbtn01_on.png");
+            return false;
+        });
+        $(".guidemap-btn01").mouseout(function(){
+            $(".guide-menu01").css("color","#fff");
+            $("#guidmap-img01").attr("src","images/mapbtn01.png");
+            return false;
+        });
+
+
+        $(".guide-menu01").mouseover(function(){
+            $(".guide-menu01").css("color","#e2de66");
+            $("#guidmap-img01").attr("src","images/mapbtn01_on.png");
+            return false;
+        });
+        $(".guide-menu01").mouseout(function(){
+            $(".guide-menu01").css("color","#fff");
+            $("#guidmap-img01").attr("src","images/mapbtn01.png");
+            return false;
+        });
+
+        // 코스버튼2
+        $(".guidemap-btn02").mouseover(function(){
+            $(".guide-menu02").css("color","#e2de66");
+            $("#guidmap-img02").attr("src","images/mapbtn02_on.png");
+            return false;
+        });
+        $(".guidemap-btn02").mouseout(function(){
+            $(".guide-menu02").css("color","#fff");
+            $("#guidmap-img02").attr("src","images/mapbtn02.png");
+            return false;
+        });
+        
+
+        $(".guide-menu02").mouseover(function(){
+            $(".guide-menu02").css("color","#e2de66");
+            $("#guidmap-img02").attr("src","images/mapbtn02_on.png");
+            return false;
+        });
+        $(".guide-menu02").mouseout(function(){
+            $(".guide-menu02").css("color","#fff");
+            $("#guidmap-img02").attr("src","images/mapbtn02.png");
+            return false;
+        });
+
+        // 코스버튼3
+        $(".guidemap-btn03").mouseover(function(){
+            $(".guide-menu03").css("color","#e2de66");
+            $("#guidmap-img03").attr("src","images/mapbtn03_on.png");
+            return false;
+        });
+        $(".guidemap-btn03").mouseout(function(){
+            $(".guide-menu03").css("color","#fff");
+            $("#guidmap-img03").attr("src","images/mapbtn03.png");
+            return false;
+        });
+
+        $(".guide-menu03").mouseover(function(){
+            $(".guide-menu03").css("color","#e2de66");
+            $("#guidmap-img03").attr("src","images/mapbtn03_on.png");
+            return false;
+        });
+        $(".guide-menu03").mouseout(function(){
+            $(".guide-menu03").css("color","#fff");
+            $("#guidmap-img03").attr("src","images/mapbtn03.png");
+            return false;
+        });
+
+        // 코스버튼4
+        $(".guidemap-btn04").mouseover(function(){
+            $(".guide-menu04").css("color","#e2de66");
+            $("#guidmap-img04").attr("src","images/mapbtn04_on.png");
+            return false;
+        });
+        $(".guidemap-btn04").mouseout(function(){
+            $(".guide-menu04").css("color","#fff");
+            $("#guidmap-img04").attr("src","images/mapbtn04.png");
+            return false;
+        });
+
+        $(".guide-menu04").mouseover(function(){
+            $(".guide-menu04").css("color","#e2de66");
+            $("#guidmap-img04").attr("src","images/mapbtn04_on.png");
+            return false;
+        });
+        $(".guide-menu04").mouseout(function(){
+            $(".guide-menu04").css("color","#fff");
+            $("#guidmap-img04").attr("src","images/mapbtn04.png");
+            return false;
+        });
+
+        
+        // 코스버튼5
+        $(".guidemap-btn05").mouseover(function(){
+            $(".guide-menu05").css("color","#e2de66");
+            $("#guidmap-img05").attr("src","images/mapbtn05_on.png");
+            return false;
+        });
+        $(".guidemap-btn05").mouseout(function(){
+            $(".guide-menu05").css("color","#fff");
+            $("#guidmap-img05").attr("src","images/mapbtn05.png");
+            return false;
+        });
+
+        $(".guide-menu05").mouseover(function(){
+            $(".guide-menu05").css("color","#e2de66");
+            $("#guidmap-img05").attr("src","images/mapbtn05_on.png");
+            return false;
+        });
+        $(".guide-menu05").mouseout(function(){
+            $(".guide-menu05").css("color","#fff");
+            $("#guidmap-img05").attr("src","images/mapbtn05.png");
+            return false;
+        });
+
+
+        // 코스버튼6
+        $(".guidemap-btn06").mouseover(function(){
+            $(".guide-menu06").css("color","#e2de66");
+            $("#guidmap-img06").attr("src","images/mapbtn06_on.png");
+            return false;
+        });
+        $(".guidemap-btn06").mouseout(function(){
+            $(".guide-menu06").css("color","#fff");
+            $("#guidmap-img06").attr("src","images/mapbtn06.png");
+            return false;
+        });
+
+        $(".guide-menu06").mouseover(function(){
+            $(".guide-menu06").css("color","#e2de66");
+            $("#guidmap-img06").attr("src","images/mapbtn06_on.png");
+            return false;
+        });
+        $(".guide-menu06").mouseout(function(){
+            $(".guide-menu06").css("color","#fff");
+            $("#guidmap-img06").attr("src","images/mapbtn06.png");
+            return false;
+        });
+
+
+        // 코스버튼7
+        $(".guidemap-btn07").mouseover(function(){
+            $(".guide-menu07").css("color","#e2de66");
+            $("#guidmap-img07").attr("src","images/mapbtn07_on.png");
+            return false;
+        });
+        $(".guidemap-btn07").mouseout(function(){
+            $(".guide-menu07").css("color","#fff");
+            $("#guidmap-img07").attr("src","images/mapbtn07.png");
+            return false;
+        });
+        
+        $(".guide-menu07").mouseover(function(){
+            $(".guide-menu07").css("color","#e2de66");
+            $("#guidmap-img07").attr("src","images/mapbtn07_on.png");
+            return false;
+        });
+        $(".guide-menu07").mouseout(function(){
+            $(".guide-menu07").css("color","#fff");
+            $("#guidmap-img07").attr("src","images/mapbtn07.png");
+            return false;
+        });
+
+
+        // 코스버튼8
+        $(".guidemap-btn08").mouseover(function(){
+            $(".guide-menu08").css("color","#e2de66");
+            $("#guidmap-img08").attr("src","images/mapbtn08_on.png");
+            return false;
+        });
+        $(".guidemap-btn08").mouseout(function(){
+            $(".guide-menu08").css("color","#fff");
+            $("#guidmap-img08").attr("src","images/mapbtn08.png");
+            return false;
+        });
+
+        $(".guide-menu08").mouseover(function(){
+            $(".guide-menu08").css("color","#e2de66");
+            $("#guidmap-img08").attr("src","images/mapbtn08_on.png");
+            return false;
+        });
+        $(".guide-menu08").mouseout(function(){
+            $(".guide-menu08").css("color","#fff");
+            $("#guidmap-img08").attr("src","images/mapbtn08.png");
+            return false;
+        });
+
+
+        // 코스버튼9
+        $(".guidemap-btn09").mouseover(function(){
+            $(".guide-menu09").css("color","#e2de66");
+            $("#guidmap-img09").attr("src","images/mapbtn09_on.png");
+            return false;
+        });
+        $(".guidemap-btn09").mouseout(function(){
+            $(".guide-menu09").css("color","#fff");
+            $("#guidmap-img09").attr("src","images/mapbtn09.png");
+            return false;
+        });
+
+        $(".guide-menu09").mouseover(function(){
+            $(".guide-menu09").css("color","#e2de66");
+            $("#guidmap-img09").attr("src","images/mapbtn09_on.png");
+            return false;
+        });
+        $(".guide-menu09").mouseout(function(){
+            $(".guide-menu09").css("color","#fff");
+            $("#guidmap-img09").attr("src","images/mapbtn09.png");
+            return false;
+        });
+
+        // 코스버튼10
+        $(".guidemap-btn10").mouseover(function(){
+            $(".guide-menu10").css("color","#e2de66");
+            $("#guidmap-img10").attr("src","images/mapbtn10_on.png");
+            return false;
+        });
+        $(".guidemap-btn10").mouseout(function(){
+            $(".guide-menu10").css("color","#fff");
+            $("#guidmap-img10").attr("src","images/mapbtn10.png");
+            return false;
+        });
+        
+        $(".guide-menu10").mouseover(function(){
+            $(".guide-menu10").css("color","#e2de66");
+            $("#guidmap-img10").attr("src","images/mapbtn10_on.png");
+            return false;
+        });
+        $(".guide-menu10").mouseout(function(){
+            $(".guide-menu10").css("color","#fff");
+            $("#guidmap-img10").attr("src","images/mapbtn10.png");
+            return false;
+        });
+
+
+        // 코스버튼11
+        $(".guidemap-btn11").mouseover(function(){
+            $(".guide-menu11").css("color","#e2de66");
+            $("#guidmap-img11").attr("src","images/mapbtn11_on.png");
+            return false;
+        });
+        $(".guidemap-btn11").mouseout(function(){
+            $(".guide-menu11").css("color","#fff");
+            $("#guidmap-img11").attr("src","images/mapbtn11.png");
+            return false;
+        });
+        
+        $(".guide-menu11").mouseover(function(){
+            $(".guide-menu11").css("color","#e2de66");
+            $("#guidmap-img11").attr("src","images/mapbtn11_on.png");
+            return false;
+        });
+        $(".guide-menu11").mouseout(function(){
+            $(".guide-menu11").css("color","#fff");
+            $("#guidmap-img11").attr("src","images/mapbtn11.png");
+            return false;
+        });
+
+
+        // 코스버튼12
+        $(".guidemap-btn12").mouseover(function(){
+            $(".guide-menu12").css("color","#e2de66");
+            $("#guidmap-img12").attr("src","images/mapbtn12_on.png");
+            return false;
+        });
+        $(".guidemap-btn12").mouseout(function(){
+            $(".guide-menu12").css("color","#fff");
+            $("#guidmap-img12").attr("src","images/mapbtn12.png");
+            return false;
+        });
+        
+        $(".guide-menu12").mouseover(function(){
+            $(".guide-menu12").css("color","#e2de66");
+            $("#guidmap-img12").attr("src","images/mapbtn12_on.png");
+            return false;
+        });
+        $(".guide-menu12").mouseout(function(){
+            $(".guide-menu12").css("color","#fff");
+            $("#guidmap-img12").attr("src","images/mapbtn12.png");
+            return false;
+        });
+
+        // 코스버튼13
+        $(".guidemap-btn13").mouseover(function(){
+            $(".guide-menu13").css("color","#e2de66");
+            $("#guidmap-img13").attr("src","images/mapbtn13_on.png");
+            return false;
+        });
+        $(".guidemap-btn13").mouseout(function(){
+            $(".guide-menu13").css("color","#fff");
+            $("#guidmap-img13").attr("src","images/mapbtn13.png");
+            return false;
+        });
+        
+        $(".guide-menu13").mouseover(function(){
+            $(".guide-menu13").css("color","#e2de66");
+            $("#guidmap-img13").attr("src","images/mapbtn13_on.png");
+            return false;
+        });
+        $(".guide-menu13").mouseout(function(){
+            $(".guide-menu13").css("color","#fff");
+            $("#guidmap-img13").attr("src","images/mapbtn13.png");
+            return false;
+        });
+
+
+        // 코스버튼14
+        $(".guidemap-btn14").mouseover(function(){
+            $(".guide-menu14").css("color","#e2de66");
+            $("#guidmap-img14").attr("src","images/mapbtn14_on.png");
+            return false;
+        });
+        $(".guidemap-btn14").mouseout(function(){
+            $(".guide-menu14").css("color","#fff");
+            $("#guidmap-img14").attr("src","images/mapbtn14.png");
+            return false;
+        });
+
+        $(".guide-menu14").mouseover(function(){
+            $(".guide-menu14").css("color","#e2de66");
+            $("#guidmap-img14").attr("src","images/mapbtn14_on.png");
+            return false;
+        });
+        $(".guide-menu14").mouseout(function(){
+            $(".guide-menu14").css("color","#fff");
+            $("#guidmap-img14").attr("src","images/mapbtn14.png");
+            return false;
+        });
+
+
+
+    });
