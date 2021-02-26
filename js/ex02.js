b316fbc5cbf810261deedfe2bf0215c9cb4f17cd
dnsgk4983 <dnsgk4983@gmail.com>
2021-02-26 17:44:54 +0900

    v.1.2.0

---
+++ js/ex02.js
@@ -0,0 +1,47 @@
+
+        $(function(){
+            $(".guidemap-btn01").click(function(e){
+                $(".guidemodal-01").css("display","block");
+                var swiper = new Swiper('.swiper-container', {
+                   spaceBetween: 30,
+                   centeredSlides: true,
+                   autoplay: {
+                     delay: 2800,
+                     disableOnInteraction: false,
+                   },
+                   pagination: {
+                       el: '.swiper-pagination',
+                       dynamicBullets: true,
+                     },
+                 spaceBetween: 30,
+                 effect: 'fade',
+               
+                 
+                 });
+               return false;               
+            });
+            $(".guide-menu01").click(function(e){
+               $(".guidemodal-01").css("display","block");
+               var swiper = new Swiper('.swiper-container', {
+                   spaceBetween: 30,
+                   centeredSlides: true,
+                   autoplay: {
+                     delay: 2800,
+                     disableOnInteraction: false,
+                   },
+                   pagination: {
+                       el: '.swiper-pagination',
+                       dynamicBullets: true,
+                     },
+                 spaceBetween: 30,
+                 effect: 'fade',
+               
+                 
+                 });
+              return false;               
+           });
+       
+           $(".madal-close-text").click(function(){
+               $(".guidemodal-01").css("display","none");
+               return false;
+           });
